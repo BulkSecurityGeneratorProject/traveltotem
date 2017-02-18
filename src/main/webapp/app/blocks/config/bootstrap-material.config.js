bd2acb412f2bf9225681e9c4d46e6e3c9e9d4253
jpetryk <jasiek.petryk@gmail.com>
2017-02-18 22:12:49 +0100

    introduced bootstrap material

---
+++ src/main/webapp/app/blocks/config/bootstrap-material.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('traveltotemApp')
+        .config(bootstrapMaterialDesignConfig);
+
+    bootstrapMaterialDesignConfig.$inject = [];
+
+    function bootstrapMaterialDesignConfig() {
+        $.material.init();
+
+    }
+})();
