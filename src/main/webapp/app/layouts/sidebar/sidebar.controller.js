cd8cadf7dbc87a4535a5f7f1780153700d5aabba
jpetryk <jasiek.petryk@gmail.com>
2017-02-19 19:14:14 +0100

    sidebar

---
+++ src/main/webapp/app/layouts/sidebar/sidebar.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('traveltotemApp')
+        .controller('SidebarController', SidebarController);
+
+    SidebarController.$inject = ['$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
+
+    function SidebarController ($state, Auth, Principal, ProfileService, LoginService) {
+        var vm = this;
+
+
+    }
+})();
